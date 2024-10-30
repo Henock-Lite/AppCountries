1314a4ae462638b7199be6835f47fd223b301b56
Henock-Lite <litehenock@gmail.com>
2024-10-30 01:47:18 +0100

    appcountries

---
+++ index.js
@@ -0,0 +1,61 @@
+let Data = [];
+let countries = document.querySelector(".countries-container");
+const inputSearch = document.getElementById("inputSearch");
+const inputRange = document.getElementById("inputRange");
+const rangeValue = document.getElementById("rangeValue");
+let trieValue = "maxToMin";
+let btnSort = document.querySelectorAll(".btnSort");
+
+async function CountriesFetch() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  Data = await res.json();
+  DisplayCountries();
+}
+
+function DisplayCountries() {
+  countries.innerHTML = Data.filter((search) =>
+    search.translations.fra.common
+      .toLowerCase()
+      .includes(inputSearch.value.toLowerCase())
+  )
+    .slice(0, inputRange.value)
+    .sort((a, b) => {
+      if (trieValue === "maxToMin") {
+        return b.population - a.population;
+      } else if (trieValue === "minToMax") {
+        return a.population - b.population;
+      } else if (trieValue === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    .map(
+      (data) =>
+        `
+    <div class ='cards'>
+    <img src='${data.flags.svg}'>
+    <h1>${data.translations.fra.common}</h1>
+    <h2>${data.capital}</h2>
+    <i>population: ${data.population.toLocaleString()}</i>
+    </div>
+    
+    `
+    )
+    .join("");
+}
+
+window.addEventListener("load", CountriesFetch);
+inputSearch.addEventListener("input", DisplayCountries);
+inputRange.addEventListener("input", () => {
+  rangeValue.textContent = inputRange.value;
+  DisplayCountries();
+});
+let i;
+
+btnSort.forEach((btnSort) => {
+  btnSort.addEventListener("click", (e) => {
+    trieValue = e.target.id;
+    DisplayCountries();
+  });
+});
